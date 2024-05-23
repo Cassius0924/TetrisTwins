d80bf4d31eef7e1d64e678bab56adf60c8b531fe
Cassius0924 <2670226747@qq.com>
2024-05-23 22:26:36 +0800

    feat: 添加SafeDeque线程安全deque类

---
+++ include/tt/util/safe_deque.h
@@ -0,0 +1,164 @@
+#ifndef SAFE_DEQUE_H
+#define SAFE_DEQUE_H
+
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+
+namespace util {
+
+template <typename T>
+class SafeDeque {
+private:
+    std::deque<T> _deque;
+    std::mutex _mutex;
+    std::condition_variable cv;
+
+    using iterator = typename std::deque<T>::iterator;
+
+public:
+    SafeDeque() = default;
+
+    ~SafeDeque() = default;
+
+    // 拷贝构造函数
+    SafeDeque(const SafeDeque &other) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque = other;
+    }
+
+    // 拷贝赋值运算符
+    SafeDeque &operator=(const SafeDeque &other) {
+        if (this != &other) {
+            std::lock_guard<std::mutex> lock1(_mutex);
+            std::lock_guard<std::mutex> lock2(other._mutex);
+            _deque = other._deque;
+        }
+        return *this;
+    }
+
+    // 移动构造函数
+    SafeDeque(const SafeDeque &&other) noexcept {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque = std::move(other);
+    }
+
+    // 移动赋值运算符
+    SafeDeque &operator=(const SafeDeque &&other) noexcept {
+        if (this != &other) {
+            std::lock_guard<std::mutex> lock1(_mutex);
+            std::lock_guard<std::mutex> lock2(other._mutex);
+            _deque = std::move(other._deque);
+        }
+        return *this;
+    }
+
+    void push_back(const T &value) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque.push_back(value);
+        cv.notify_one();
+    }
+
+    void push_back(T &&value) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque.push_back(std::move(value));
+        cv.notify_one();
+    }
+
+    void push_front(const T &value) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque.push_front(value);
+        cv.notify_one();
+    }
+
+    void push_front(T &&value) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque.push_front(std::move(value));
+        cv.notify_one();
+    }
+
+    template <typename... Args>
+    void emplace_back(Args &&...args) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque.emplace_back(std::forward<Args>(args)...);
+        cv.notify_one();
+    }
+
+    template <typename... Args>
+    void emplace_front(Args &&...args) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque.emplace_front(std::forward<Args>(args)...);
+        cv.notify_one();
+    }
+
+    void pop_back() {
+        std::unique_lock<std::mutex> lock(_mutex);
+        while (_deque.empty()) {
+            cv.wait(lock);
+        }
+        _deque.pop_back();
+    }
+
+    void pop_front() {
+        std::unique_lock<std::mutex> lock(_mutex);
+        while (_deque.empty()) {
+            cv.wait(lock);
+        }
+        _deque.pop_front();
+    }
+
+    T front() {
+        std::unique_lock<std::mutex> lock(_mutex);
+        while (_deque.empty()) {
+            cv.wait(lock);
+        }
+        return _deque.front();
+    }
+
+    T back() {
+        std::unique_lock<std::mutex> lock(_mutex);
+        while (_deque.empty()) {
+            cv.wait(lock);
+        }
+        return _deque.back();
+    }
+
+    int size() {
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _deque.size();
+    }
+
+    int empty() {
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _deque.empty();
+    }
+
+    void clear() {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque.clear();
+    }
+
+    void resize(int size) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _deque.resize(size);
+    }
+
+    T &operator[](int index) {
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _deque[index];
+    }
+
+    iterator begin() {
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _deque.begin();
+    }
+
+    iterator end() {
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _deque.end();
+    }
+};
+
+}
+
+#endif //SAFE_DEQUE_H
